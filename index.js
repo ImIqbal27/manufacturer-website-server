0c558703824a4b7f10a3636f9ed7b5540e7480db
unknown <iamhasan9501@gmail.com>
2022-05-30 16:40:01 +0600

    updated

--- index.js
+++ index.js
@@ -95,7 +95,24 @@ async function run() {
             const result = await userCollection.updateOne(filter, updateDoc);
             res.send(result);
         });
-        ///////////////////  get all  user //////////////////////////////////////////////
+        // /////////
+        // app.put('/user/admin/:email', async (req, res) => {
+        //     const email = req.params.email;
+        //     const requester = req.decoded.email;
+        //     const requesterAccount = await userCollection.findOne({ email: requester });
+        //     if (requesterAccount.role === 'admin') {
+        //         const filter = { email: email };
+        //         const updateDoc = {
+        //             $set: { role: 'admin' },
+        //         };
+        //         const result = await userCollection.updateOne(filter, updateDoc);
+        //         res.send(result);
+        //     }
+        //     else {
+        //         res.status(403).send({ message: 'forbidden' });
+        //     }
+        // });
+        // ///////////////////  get all  user //////////////////////////////////////////////
         app.get('/user', verifyJWT, async (req, res) => {
             const users = await userCollection.find().toArray();
             res.send(users);
